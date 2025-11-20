c73d8bb714e3fc6c607475fbcfbba69aa93b3834
Tarak95 <tarekrahman8295@gmail.com>
2025-11-20 11:14:46 +0600

    update something

--- src/components/UserList/UserList.jsx
+++ src/components/UserList/UserList.jsx
@@ -5,7 +5,7 @@ import swathi from "../../assets/swathi.png"
 import kiran from "../../assets/kiran.png"
 import tejesh from "../../assets/tejesh.png"
 import marvin from "../../assets/marvin.png"
-import { FaSquarePlus } from "react-icons/fa6";
+import { FaSquarePlus, FaSquareMinus } from "react-icons/fa6";
 
 import { MdAddBox } from "react-icons/md";
 import { getDatabase, ref, onValue, set, push } from "firebase/database";
@@ -15,17 +15,18 @@ import { useSelector } from 'react-redux';
 const UserList = () => {
 
      const data = useSelector((selector) => (selector?.userInfo?.value?.user))
-  console.log(data?.uid,'UID');
+     
 
 
       const db = getDatabase();
-  const [userList, setUserList] = useState([]); 
+      const [userList, setUserList] = useState([]); 
 
   useEffect(() => {
-    const useRef = ref(db, "users");
-    onValue(useRef, (snapshot) => {
+    const userRef = ref(db, "users");
+    onValue(userRef, (snapshot) => {
       let arr = [];
       snapshot.forEach((item) => {
+        if (data?.uid !== item.key)
         arr.push({...item.val(), userid: item.key});
       });
       setUserList(arr);
@@ -35,49 +36,94 @@ const UserList = () => {
 
 
      const handleFriendRequest = (item) => {
-        console.log("ok", item)
-        set(push(ref(db,'friendRequest/')), {
-            senderName: data.displayName,
-            senderId:data.uid,
-            receiverName: item.username
-        });
+        console.log( item)
+       set(push(ref(db,'friendRequest/')), {
+       senderName: data.displayName,
+       senderId: data?.uid,
+       receiverName: item.username,
+       receiverId: item.userid
+});
+
 
     
   }
+
+  const [friendRequestList, setFriendRequestList] = useState([])
+  useEffect(() =>{
+    const friendRequestRef = ref(db, "friendRequest")
+    onValue(friendRequestRef, (snapshot)=>{
+        let arr = []
+        snapshot.forEach((item) => {
+            arr.push(item.val().receiverId + item.val().senderId)
+        })
+        setFriendRequestList(arr)
+    })
+  }, [])
+
+
+ const [friendList, setFriendList] = useState([]);
+
+useEffect(() => {
+    const friendRef = ref(db, "friend");
+    onValue(friendRef, (snapshot) => {
+        let arr = [];
+        snapshot.forEach((item) => {
+            arr.push(item.val().receiverId + item.val().senderId);
+        });
+        setFriendList(arr);
+    });
+}, []);
+
 
 
 
 
     return (
-        <div className='shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)] p-[20px] rounded-[20px] ml-[43px] h-[347px] overflow-y-scroll'>
-            <div className='flex items-center'>
+        <div className='shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)] rounded-[20px] pt-[13px] pb-[21px] pl-[20px] pr-[22px]'>
+            <div className='flex justify-between items-center'>
                 <h1 className='font-primary font-semibold text-[20px]'>User List</h1>
-                <HiOutlineDotsVertical className='ml-[201px]' />
+                <HiOutlineDotsVertical className='text-xl' />
             </div>
 
-             {
-                userList.map((user)=>
+            <div className='px-[10px] h-[390px] overflow-y-scroll'>
 
-                       <div className='relative flex items-center mt-[29px]'>
-                <div>
-                    <img src={tejesh} alt="" />
-                </div>
+                {
+                    userList.map((user) => (
+                        <div className='flex justify-between items-center mt-[17px] border-b pb-[13px] border-black/25'>
+                            <div className='flex items-center'>
+                                <img src={kiran} alt="" />
 
-                <div className='ml-[14px]'>
-                    <p className='font-primary font-semibold text-[14px]'>{user.username}</p>
-                    <p className='font-primary font-medium text-[10px] text-[#4D4D4D]/75'>{user.email}</p>
-                </div>
+                                <div className='ml-[14px]'>
+                                    <h3 className='font-semibold font-primary text-[18px]'>{user.username}</h3>
+                                    <p className='font-primary font-medium text-[14px] text-[#4D4D4D]/75'>{user.email}</p>
+                                </div>
+                            </div>
 
-                <div className='ml-[89px]'>
-                    <FaSquarePlus onClick={() => handleFriendRequest(user)} className='text-[30px]' />
-                </div>
+                            {
+                                friendList.includes(data?.uid + user.userid) ||
+                                    friendList.includes(user.userid + data?.uid) ? (
+                                    <p className='text-green-600 font-semibold font-primary'>Friend</p>
 
-               
+                                )
+                                    :
+                                    friendRequestList.includes(data?.uid + user.userid) ||
+                                        friendRequestList.includes(user.userid + data?.uid)
+                                        ?
+                                        <FaSquareMinus className='text-[30px]' />
+                                        :
+                                        <FaSquarePlus onClick={() => handleFriendRequest(user)} className='text-[30px]' />
+
+
+                            }
+
+
+
+                        </div>
+
+                    ))
+                }
+
             </div>
-                )
-             }
-           
-         
         </div>
     )
 }

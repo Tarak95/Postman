36e79b7ed51e29a1f2ba6d579d86ce4e03e33944
Tarak95 <tarekrahman8295@gmail.com>
2025-12-04 15:53:53 +0600

    New update added

--- src/components/UserList/UserList.jsx
+++ src/components/UserList/UserList.jsx
@@ -92,6 +92,22 @@ const UserList = () => {
 
 
 
+    const [filterUserList, setFilterUserList] = useState([])
+    const handleSearch = (e) => {
+        let arr = []
+        if (e.target.value.length == 0) {
+            setFilterUserList([])
+        } else {
+            userList.filter((item) => {
+                if (item.username.toLowerCase().includes(e.target.value.toLowerCase())) {
+                    arr.push(item)
+                    setFilterUserList(arr)
+                }
+            })
+        }
+    }
+
+
 
     return (
         <div className='shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)] rounded-[20px] pt-[13px] pb-[21px] pl-[20px] pr-[22px]'>
@@ -100,49 +116,101 @@ const UserList = () => {
                 <HiOutlineDotsVertical className='text-xl' />
             </div>
 
+            <div>
+                <input
+                    onChange={handleSearch} type="text"
+                    className='border py-[5px] px-[10px] w-[350px]' />
+            </div>
+
             <div className='px-[10px] h-[390px] overflow-y-scroll'>
 
                 {
-                    userList.map((user) => (
-                        <div className='flex justify-between items-center mt-[17px] border-b pb-[13px] border-black/25'>
-                            <div className='flex items-center'>
-                                <img src={marvin} alt="" />
+                    filterUserList.length > 0 ?
 
-                                <div className='ml-[14px]'>
-                                    <h3 className='font-semibold font-primary text-[18px]'>{user.username}</h3>
-                                    <p className='font-primary font-medium text-[14px] text-[#4D4D4D]/75'>{user.email}</p>
+                        filterUserList.map((user) => (
+                            <div className='flex justify-between items-center mt-[17px] border-b pb-[13px] border-black/25'>
+                                <div className='flex items-center'>
+                                    <img src={tejesh} alt="" />
+
+                                    <div className='ml-[14px]'>
+                                        <h3 className='font-semibold font-primary text-[18px]'>{user.username}</h3>
+                                        <p className='font-primary font-medium text-[14px] text-[#4D4D4D]/75'>{user.email}</p>
+                                    </div>
                                 </div>
-                            </div>
 
-                            {
-                                blockList.includes(data?.uid + user.userid) ||
-                                    blockList.includes(user.userid + data?.uid) ? (
-                                    <p className='text-red-600 font-semibold font-primary'>BLOCKED</p>
-                                )
-                                    :
-
-
-                                    friendList.includes(data?.uid + user.userid) ||
-                                        friendList.includes(user.userid + data?.uid) ? (
-                                        <p className='text-green-600 font-semibold font-primary'>Friend</p>
-
+                                {
+                                    blockList.includes(data?.uid + user.userid) ||
+                                        blockList.includes(user.userid + data?.uid) ? (
+                                        <p className='text-red-600 font-semibold font-primary'>BLOCKED</p>
                                     )
                                         :
-                                        friendRequestList.includes(data?.uid + user.userid) ||
-                                            friendRequestList.includes(user.userid + data?.uid)
-                                            ?
-                                            <FaSquareMinus className='text-[30px]' />
+
+
+                                        friendList.includes(data?.uid + user.userid) ||
+                                            friendList.includes(user.userid + data?.uid) ? (
+                                            <p className='text-green-600 font-semibold font-primary'>Friend</p>
+
+                                        )
                                             :
-                                            <FaSquarePlus onClick={() => handleFriendRequest(user)} className='text-[30px]' />
+                                            friendRequestList.includes(data?.uid + user.userid) ||
+                                                friendRequestList.includes(user.userid + data?.uid)
+                                                ?
+                                                <FaSquareMinus className='text-[30px]' />
+                                                :
+                                                <FaSquarePlus onClick={() => handleFriendRequest(user)} className='text-[30px]' />
 
 
-                            }
+                                }
 
 
 
-                        </div>
+                            </div>
 
-                    ))
+                        ))
+
+                        :
+
+
+                        userList.map((user) => (
+                            <div className='flex justify-between items-center mt-[17px] border-b pb-[13px] border-black/25'>
+                                <div className='flex items-center'>
+                                    <img src={tejesh} alt="" />
+
+                                    <div className='ml-[14px]'>
+                                        <h3 className='font-semibold font-primary text-[18px]'>{user.username}</h3>
+                                        <p className='font-primary font-medium text-[14px] text-[#4D4D4D]/75'>{user.email}</p>
+                                    </div>
+                                </div>
+
+                                {
+                                    blockList.includes(data?.uid + user.userid) ||
+                                        blockList.includes(user.userid + data?.uid) ? (
+                                        <p className='text-red-600 font-semibold font-primary'>BLOCKED</p>
+                                    )
+                                        :
+
+
+                                        friendList.includes(data?.uid + user.userid) ||
+                                            friendList.includes(user.userid + data?.uid) ? (
+                                            <p className='text-green-600 font-semibold font-primary'>Friend</p>
+
+                                        )
+                                            :
+                                            friendRequestList.includes(data?.uid + user.userid) ||
+                                                friendRequestList.includes(user.userid + data?.uid)
+                                                ?
+                                                <FaSquareMinus className='text-[30px]' />
+                                                :
+                                                <FaSquarePlus onClick={() => handleFriendRequest(user)} className='text-[30px]' />
+
+
+                                }
+
+
+
+                            </div>
+
+                        ))
                 }
 
             </div>
